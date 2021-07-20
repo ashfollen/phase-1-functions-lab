0ac82df89ed0781143f10692bbecde6dcd1ff39a
Ashley Follen <ashleyfollen@outlook.com>
2021-07-20 10:47:58 -0600

    commit tree 75713149ff99cd67bbe276177811ce10a65809f8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const hqBlock = 42;
+function distanceFromHqInBlocks(pickupBlock) {
+    return Math.abs(pickupBlock - hqBlock);
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+    return distanceFromHqInBlocks(pickupBlock) * 264;
+}
+
+function distanceTravelledInFeet(pickupBlock, dropoffBlock) {
+    return Math.abs((pickupBlock - dropoffBlock) * 264);
+}
+
+let farePrice;
+function calculatesFarePrice(pickupBlock, dropoffBlock) {
+    if (distanceTravelledInFeet(pickupBlock, dropoffBlock) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(pickupBlock, dropoffBlock) > 400 && distanceTravelledInFeet(pickupBlock, dropoffBlock) <= 2000) {
+        return (distanceTravelledInFeet(pickupBlock, dropoffBlock) - 400) * .02;
+    } else if (distanceTravelledInFeet(pickupBlock, dropoffBlock) > 2000 && distanceTravelledInFeet(pickupBlock, dropoffBlock)<= 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(pickupBlock, dropoffBlock) > 2500) {
+        return "cannot travel that far";
+    }
+}
